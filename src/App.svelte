3516a09d4058d7edf1c83eaba2267e6a297bde56
Simon Dagfinrud <17279882+s-rd@users.noreply.github.com>
2021-11-14 23:10:32 +0100

    Add crude language selector + detection

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,61 @@
 <script>
 	import { onMount } from 'svelte'
-	import translations from 'codeofdesign'
+	import versions from 'codeofdesign'
 
 	let content
+	let lang
+	let detectedLang
+	let translations = []
+
+	const getTranslation = id => {
+		lang = id
+		return versions.find(t => t.metadata.id === id) || false
+	}
+
+	const handleChange = () => {
+		if (lang === 'contribute') {
+			window.open('https://github.com/codeofdesign/code#contributing')
+			lang = detectedLang
+		}
+		content = getTranslation(lang).html
+	}
+
+	const getUserLanguage = () => {
+		// Not very smart way of detecting. @TODO: make something better
+		let loc = 'en_US'
+
+		try {
+			let userLangs = window.navigator.languages
+			loc = userLangs.find(l => {
+				return translations.find(t => t === l)
+			})
+		} catch (e) {
+			console.error(e)
+		} finally {
+			return loc
+		}
+	}
 
 	onMount(() => {
-		const translation = translations.find(t => t.metadata.id === 'en_US')
+		translations = versions
+			.filter(f => f.metadata.complete)
+			.map(f => f.metadata.id)
+
+		detectedLang = getUserLanguage()
+		const translation = getTranslation(detectedLang)
 		content = translation.html
-		console.log(translations)
 	})
 </script>
 
 <main>
+	<select bind:value={lang} on:change={handleChange}>
+		{#each versions.filter(v => v.metadata.complete) as opt}
+			<option value={opt.metadata.id}>{opt.metadata.name}</option>
+		{/each}
+		<option disabled>---</option>
+		<option value="contribute">Add translation</option>
+	</select>
+
 	{@html content}
 </main>
 
@@ -21,6 +65,11 @@
 	main {
 		font-size: clamp(2rem, 4vw, 3rem);
 		font-family: 'Inter', sans-serif;
+	}
+
+	select {
+		font-size: 0.75em;
+		margin: 0;
 	}
 
 	:global(h1:first-child) {
