7233526483e377c31769e156e5163c55499c7cbe
Simon Dagfinrud <17279882+s-rd@users.noreply.github.com>
2022-12-04 23:22:08 +0100

    Use local inter

--- src/App.svelte
+++ src/App.svelte
@@ -87,8 +87,6 @@
 </footer>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap');
-
 	select {
 		font-size: clamp(20px, 0.5em, 40px);
 		margin: 0;
